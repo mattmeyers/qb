4bc2b169d505e5fab5f0cb635eda98a744625ad0
Matt Meyers <matt.meyers@mcgstrategic.com>
2019-10-15 15:55:53 -0400

    Added joins.

---
+++ join.go
@@ -0,0 +1,52 @@
+package qb
+
+import (
+	"fmt"
+	"strings"
+)
+
+type joinType int
+
+const (
+	innerJoin joinType = iota
+	leftOuterJoin
+	rightOuterJoin
+	fullOuterJoin
+	crossJoin
+)
+
+func (jt joinType) toString() string {
+	switch jt {
+	case innerJoin:
+		return "INNER JOIN"
+	case leftOuterJoin:
+		return "LEFT OUTER JOIN"
+	case rightOuterJoin:
+		return "RIGHT OUTER JOIN"
+	case fullOuterJoin:
+		return "FULL OUTER JOIN"
+	case crossJoin:
+		return "CROSS JOIN"
+	}
+	return ""
+}
+
+type join struct {
+	joinType  joinType
+	table     string
+	condition string
+}
+
+type joinClause []join
+
+func newJoin(joinType joinType, table, condition string) join {
+	return join{joinType, table, condition}
+}
+
+func (jc joinClause) String() (string, []interface{}, error) {
+	parts := make([]string, len(jc))
+	for i, j := range jc {
+		parts[i] = fmt.Sprintf("%s %s ON %s", j.joinType.toString(), j.table, j.condition)
+	}
+	return strings.Join(parts, " "), nil, nil
+}
---
+++ join_test.go
@@ -0,0 +1,49 @@
+package qb
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_joinClause_String(t *testing.T) {
+	tests := []struct {
+		name    string
+		jc      joinClause
+		want    string
+		want1   []interface{}
+		wantErr bool
+	}{
+		{
+			name:    "Single inner join",
+			jc:      joinClause{join{innerJoin, "b", "a.id=b.a_id"}},
+			want:    "INNER JOIN b ON a.id=b.a_id",
+			want1:   nil,
+			wantErr: false,
+		},
+		{
+			name: "Multiple inner joins",
+			jc: joinClause{
+				join{innerJoin, "b", "a.id=b.a_id"},
+				join{innerJoin, "c", "b.id=c.b_id"},
+			},
+			want:    "INNER JOIN b ON a.id=b.a_id INNER JOIN c ON b.id=c.b_id",
+			want1:   nil,
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, got1, err := tt.jc.String()
+			if (err != nil) != tt.wantErr {
+				t.Errorf("joinClause.String() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("joinClause.String() got = %v, want %v", got, tt.want)
+			}
+			if !reflect.DeepEqual(got1, tt.want1) {
+				t.Errorf("joinClause.String() got1 = %v, want %v", got1, tt.want1)
+			}
+		})
+	}
+}
